5e19e18fc85e0e4e182a0e2b079b573ee2efdbc7
Olya-Myalo <olyamyalo@mail.ru>
2023-08-22 20:11:22 +0300

    выполнена 1-я задача

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,31 +1,38 @@
 import './Nav.css';
+import { useState } from 'react';
 
 const Nav = () => {
-    return (
-        <nav className="main__nav nav">
+  const [visible, setVisible] = useState(true);
+
+  const toggleVisibility = () => setVisible(!visible);
+
+  return (
+    <nav className="main__nav nav">
         <div className="nav__logo logo">
           <img className="logo__image" src="img/logo.png" alt="logo" />
         </div>
-        <div className="nav__burger burger">
+        <div onClick={toggleVisibility} className="nav__burger burger">
           <span className="burger__line"></span>
           <span className="burger__line"></span>
           <span className="burger__line"></span>
         </div>
+      {visible && (
         <div className="nav__menu menu">
-          <ul className="menu__list">
-            <li className="menu__item">
-              <a href="/" className="menu__link">Главная</a>
-            </li>
-            <li className="menu__item">
-              <a href="/playlist" className="menu__link">Мой плейлист</a>
-            </li>
-            <li className="menu__item">
-              <a href="/signin" className="menu__link">Войти</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
-}
+        <ul className="menu__list">
+          <li className="menu__item">
+            <a href="/" className="menu__link">Главная</a>
+          </li>
+          <li className="menu__item">
+            <a href="/playlist" className="menu__link">Мой плейлист</a>
+          </li>
+          <li className="menu__item">
+            <a href="/signin" className="menu__link">Войти</a>
+          </li>
+        </ul>
+      </div>
+      )}
+    </nav>
+  );
+};
 
 export default Nav;
